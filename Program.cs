ab2bbd70b8d58c89d910d9b5782404ca296d0f98
rduda <91785043+UnknownThink@users.noreply.github.com>
2021-10-02 14:18:47 +0200

    zmianki

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace pierwszy1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aplikacja Emerytura");
+            Console.Write("Podaj swoje imię: ");
+            string imie = Console.ReadLine();
+            Console.WriteLine($"Hello {imie}");
+        }
+    }
+}
